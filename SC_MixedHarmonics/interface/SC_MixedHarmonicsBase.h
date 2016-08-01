e2e3988829a9d6529d5e209b310ae6ab77e34539
KongTu <kongkong@kongminicable.dyndns.cern.ch>
2016-08-01 11:28:32 +0200

    getByToken setup

---
+++ SC_MixedHarmonics/interface/SC_MixedHarmonicsBase.h
@@ -0,0 +1,222 @@
+#ifndef SC_MixedHarmonicsBase_
+#define SC_MixedHarmonicsBase_
+
+
+// system include files
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <sstream>
+
+
+#include <TMath.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TH3D.h>
+#include <TNtuple.h>
+#include <TFile.h>
+#include <TROOT.h>
+#include <TSystem.h>
+#include <TString.h>
+#include <TCanvas.h>
+#include <TVector3.h>
+#include <TRandom.h>
+#include <TNtuple.h>
+#include <TGraph.h>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/JetReco/interface/PFJet.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidateFwd.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/TrackReco/interface/DeDxData.h"
+
+#include <Math/Functions.h>
+#include <Math/SVector.h>
+#include <Math/SMatrix.h>
+
+//////////////////////////////////////////////
+// CMSSW user include files
+#include "DataFormats/Common/interface/DetSetAlgorithm.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
+#include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerLayerIdAccessor.h"
+
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertex.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "SimDataFormats/TrackingHit/interface/PSimHit.h"
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+#include "SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h"
+
+#include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
+
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetup.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapRecord.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMap.h"
+
+#include "L1Trigger/GlobalTrigger/interface/L1GlobalTrigger.h"
+
+#include "DataFormats/Math/interface/Point3D.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "DataFormats/RecoCandidate/interface/TrackAssociation.h"
+#include "SimTracker/TrackAssociation/interface/TrackAssociatorByHits.h"
+#include "DataFormats/TrackReco/interface/DeDxData.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+// Heavyion
+#include "DataFormats/HeavyIonEvent/interface/Centrality.h"
+#include "RecoHI/HiCentralityAlgos/interface/CentralityProvider.h"
+
+// Particle Flow
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowReco/interface/PFBlock.h"
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
+#include "DataFormats/ParticleFlowReco/interface/PFClusterFwd.h"
+
+// Vertex significance
+#include "RecoBTag/SecondaryVertex/interface/SecondaryVertex.h"
+
+// Root include files
+#include "TTree.h"
+//
+// Track Matching and fake rate calculations     
+//#include "RiceHIG/V0Analysis/interface/V0Validator.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+
+using namespace std;
+using namespace reco;
+using namespace edm;
+
+
+class SC_MixedHarmonics : public edm::EDAnalyzer {
+   public:
+      explicit SC_MixedHarmonics(const edm::ParameterSet&);
+      ~SC_MixedHarmonics();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+      virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+      // ----------member data ---------------------------
+      edm::EDGetTokenT<reco::Centrality> centralityToken_;
+      edm::EDGetTokenT<int> centralityBinToken_;
+
+      edm::EDGetTokenT<reco::VertexCollection> vertexSrc_;
+      edm::EDGetTokenT<reco::TrackCollection> trackSrc_;
+      edm::EDGetTokenT<CaloTowerCollection> towerSrc_;
+      edm::EDGetTokenT<reco::GenParticleCollection> genSrc_;
+
+      edm::InputTag vertexName_;
+      edm::InputTag trackName_;
+      edm::InputTag pfCandName_;
+      edm::InputTag towerName_;
+
+      //correction table
+      TH2D* effTable;
+
+      TH1D* Ntrk;
+      TH1D* vtxZ;
+      TH1D* trkPhi;
+      TH1D* hfPhi;
+      TH1D* trkPt;
+      TH1D* trk_eta;
+      TH1D* cbinHist;
+      TH1D* q2_mag;
+
+      int Nmin_;
+      int Nmax_;
+
+      double etaTracker_;
+      
+      double etaLowHF_;
+      double etaHighHF_;
+      
+      double vzLow_;
+      double vzHigh_;
+      
+      double ptLow_;
+      double ptHigh_;
+      
+      double offlineptErr_;
+      double offlineDCA_;
+      double offlineChi2_;
+      double offlinenhits_;
+
+      bool useCentrality_;
+      bool reverseBeam_;
+      bool doEffCorrection_;
+
+      std::vector<double> etaBins_;
+      std::vector<double> dEtaBins_;
+      std::vector<double> ptBins_;
+
+};
+
+#endif
--- SC_MixedHarmonics/plugins/SC_MixedHarmonics.cc
+++ SC_MixedHarmonics/plugins/SC_MixedHarmonics.cc
@@ -17,59 +17,46 @@
 //
 
 
-// system include files
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
-
-// If the analyzer does not use TFileService, please remove
-// the template argument to the base class so the class inherits
-// from  edm::one::EDAnalyzer<> and also remove the line from
-// constructor "usesResource("TFileService");"
-// This will improve performance in multithreaded jobs.
-
-class SC_MixedHarmonics : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
-   public:
-      explicit SC_MixedHarmonics(const edm::ParameterSet&);
-      ~SC_MixedHarmonics();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+#include "SC_MixedHarmonics/SC_MixedHarmonics/interface/SC_MixedHarmonicsBase.h"
 
 
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      // ----------member data ---------------------------
-};
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
 SC_MixedHarmonics::SC_MixedHarmonics(const edm::ParameterSet& iConfig)
-
 {
-   //now do what ever initialization is needed
-   usesResource("TFileService");
+
+  trackName_  =  iConfig.getParameter<edm::InputTag>("trackName");
+  vertexName_ =  iConfig.getParameter<edm::InputTag>("vertexName");
+  towerName_ =  iConfig.getParameter<edm::InputTag>("towerName");
+
+  trackSrc_ = consumes<reco::TrackCollection>(trackName_);
+  vertexSrc_ = consumes<reco::VertexCollection>(vertexName_);
+  towerSrc_ = consumes<CaloTowerCollection>(towerName_);
+
+  Nmin_ = iConfig.getUntrackedParameter<int>("Nmin");
+  Nmax_ = iConfig.getUntrackedParameter<int>("Nmax");
+  
+  useCentrality_ = iConfig.getUntrackedParameter<bool>("useCentrality");
+  reverseBeam_ = iConfig.getUntrackedParameter<bool>("reverseBeam");
+  doEffCorrection_ = iConfig.getUntrackedParameter<bool>("doEffCorrection");
+
+  etaTracker_ = iConfig.getUntrackedParameter<double>("etaTracker");
+  
+  etaLowHF_ = iConfig.getUntrackedParameter<double>("etaLowHF");
+  etaHighHF_ = iConfig.getUntrackedParameter<double>("etaHighHF");
+  
+  vzLow_ = iConfig.getUntrackedParameter<double>("vzLow");
+  vzHigh_ = iConfig.getUntrackedParameter<double>("vzHigh");
+  
+  ptLow_ = iConfig.getUntrackedParameter<double>("ptLow");
+  ptHigh_ = iConfig.getUntrackedParameter<double>("ptHigh");
+
+  offlineptErr_ = iConfig.getUntrackedParameter<double>("offlineptErr", 0.0);
+  offlineDCA_ = iConfig.getUntrackedParameter<double>("offlineDCA", 0.0);
+  offlineChi2_ = iConfig.getUntrackedParameter<double>("offlineChi2", 0.0);
+  offlinenhits_ = iConfig.getUntrackedParameter<double>("offlinenhits", 0.0);
+  
+  etaBins_ = iConfig.getUntrackedParameter<std::vector<double>>("etaBins");
+  dEtaBins_ = iConfig.getUntrackedParameter<std::vector<double>>("dEtaBins");
+  ptBins_ = iConfig.getUntrackedParameter<std::vector<double>>("ptBins");
 
 }
 
@@ -91,19 +78,55 @@ SC_MixedHarmonics::~SC_MixedHarmonics()
 void
 SC_MixedHarmonics::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
+  using namespace std;
 
+  edm::Handle<reco::VertexCollection> vertices;
+  iEvent.getByToken(vertexSrc_,vertices);
+  double bestvz=-999.9, bestvx=-999.9, bestvy=-999.9;
+  double bestvzError=-999.9, bestvxError=-999.9, bestvyError=-999.9;
+  const reco::Vertex & vtx = (*vertices)[0];
+  bestvz = vtx.z(); 
+  bestvx = vtx.x(); 
+  bestvy = vtx.y();
+  bestvzError = vtx.zError(); 
+  bestvxError = vtx.xError(); 
+  bestvyError = vtx.yError();
 
+  //first selection; vertices
+  if( fabs(bestvz) < vzLow_ || fabs(bestvz) > vzHigh_ ) return;
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+  vtxZ->Fill( bestvz );
+
+  Handle<CaloTowerCollection> towers;
+  iEvent.getByToken(towerSrc_, towers);
+
+  Handle<reco::TrackCollection> tracks;
+  iEvent.getByToken(trackSrc_, tracks);
+
+  int total = 0;
+  for(unsigned it = 0; it < tracks->size(); it++){
+
+     const reco::Track & trk = (*tracks)[it];
+  
+     math::XYZPoint bestvtx(bestvx,bestvy,bestvz);
+
+        double dzvtx = trk.dz(bestvtx);
+        double dxyvtx = trk.dxy(bestvtx);
+        double dzerror = sqrt(trk.dzError()*trk.dzError()+bestvzError*bestvzError);
+        double dxyerror = sqrt(trk.d0Error()*trk.d0Error()+bestvxError*bestvyError); 
+
+        if(!trk.quality(reco::TrackBase::highPurity)) continue;
+        if(fabs(trk.ptError())/trk.pt() > 0.1 ) continue;
+        if(fabs(dzvtx/dzerror) > 3.0) continue;
+        if(fabs(dxyvtx/dxyerror) > 3.0) continue;
+        if(trk.pt() < 0.4 || fabs(trk.eta()) > 2.4) continue;
+        total++;//count multiplicity
+
+  }
+
+  Ntrk->Fill( total );
+  
 }
 
 
@@ -111,6 +134,12 @@ SC_MixedHarmonics::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 void 
 SC_MixedHarmonics::beginJob()
 {
+  edm::Service<TFileService> fs;
+    
+  TH1D::SetDefaultSumw2();
+
+  Ntrk = fs->make<TH1D>("Ntrk",";Ntrk",5000,0,5000);
+  vtxZ = fs->make<TH1D>("vtxZ",";vz", 400,-20,20);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -122,11 +151,6 @@ SC_MixedHarmonics::endJob()
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
 SC_MixedHarmonics::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
